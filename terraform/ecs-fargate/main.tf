9a9527d43662d0d452f224f3c3a919057aa3531a
Valdemar Zavadsky <valda@cloudfield.cz>
2023-05-13 15:40:55 +0200

    ecs first draft

---
+++ terraform/ecs-fargate/main.tf
@@ -0,0 +1,105 @@
+ locals {
+    container_name = "perftest-container"
+    container_port = 80
+    cluster_name = "perftest-cluster"
+ }
+
+module "ecs" {
+ source  = "terraform-aws-modules/ecs/aws"
+ version = "~> 4.1.3"
+
+ cluster_name = local.cluster_name
+
+ # * Allocate 20% capacity to FARGATE and then split
+ # * the remaining 80% capacity 50/50 between FARGATE
+ # * and FARGATE_SPOT.
+ fargate_capacity_providers = {
+  FARGATE = {
+   default_capacity_provider_strategy = {
+    base   = 20
+    weight = 50
+   }
+  }
+  FARGATE_SPOT = {
+   default_capacity_provider_strategy = {
+    weight = 50
+   }
+  }
+ }
+}
+
+variable "container_image" {
+ type = string
+}
+
+data "aws_iam_role" "ecs_task_execution_role" { name = "ecsTaskExecutionRole" }
+
+resource "aws_ecs_task_definition" "this" {
+ container_definitions = jsonencode([{
+  environment: [
+   { name = "NODE_ENV", value = "production" }
+  ],
+  essential = true,
+  image = var.container_image,
+  name = local.container_name,
+  portMappings = [{ containerPort = local.container_port }],
+ }])
+ cpu = "512"
+ execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+ family = "family-of-perftest-tasks"
+ memory = "1024"
+ network_mode = "awsvpc"
+ requires_compatibilities = ["FARGATE"]
+}
+
+resource "aws_ecs_service" "this" {
+ cluster = module.ecs.cluster_id
+ desired_count = 1
+ launch_type = "FARGATE"
+ name = "perftest-service"
+ task_definition = resource.aws_ecs_task_definition.this.arn
+
+ lifecycle {
+  ignore_changes = [desired_count] # Allow external changes to happen without Terraform conflicts, particularly around auto-scaling.
+ }
+
+ load_balancer {
+  container_name = local.container_name
+  container_port = local.container_port
+  target_group_arn = module.alb.target_group_arns[0]
+ }
+
+ network_configuration {
+  security_groups = [module.vpc.default_security_group_id]
+  subnets = module.vpc.private_subnets
+ }
+}
+
+resource "aws_appautoscaling_target" "this" {
+  max_capacity       = 64
+  min_capacity       = 1
+  resource_id        = "service/${module.ecs.cluster_name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "this" {
+  name               = "cpu-utilization"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.this.resource_id
+  scalable_dimension = aws_appautoscaling_target.this.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.this.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 75.0
+    scale_in_cooldown  = 5
+    scale_out_cooldown = 60
+  }
+}
+
+output "url" { value = "http://${module.alb.lb_dns_name}" }
+
+
---
+++ terraform/ecs-fargate/network.tf
@@ -0,0 +1,60 @@
+data "aws_availability_zones" "available" { state = "available" }
+module "vpc" {
+ source = "terraform-aws-modules/vpc/aws"
+ version = "~> 3.19.0"
+
+ azs = slice(data.aws_availability_zones.available.names, 0, 2) # Span subnetworks across multiple avalibility zones
+ cidr = "10.0.0.0/16"
+ create_igw = true # Expose public subnetworks to the Internet
+ enable_nat_gateway = true # Hide private subnetworks behind NAT Gateway
+ private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+ public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+module "alb" {
+ source  = "terraform-aws-modules/alb/aws"
+ version = "~> 8.4.0"
+
+ load_balancer_type = "application"
+ security_groups = [module.vpc.default_security_group_id]
+ subnets = module.vpc.public_subnets
+ vpc_id = module.vpc.vpc_id
+
+ security_group_rules = {
+  ingress_all_http = {
+   type        = "ingress"
+   from_port   = 80
+   to_port     = 80
+   protocol    = "TCP"
+   description = "Permit incoming HTTP requests from the internet"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress_all = {
+   type        = "egress"
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   description = "Permit all outgoing requests to the internet"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
+
+ http_tcp_listeners = [
+  {
+   # * Setup a listener on port 80 and forward all HTTP
+   # * traffic to target_groups[0] defined below which
+   # * will eventually point to our "Hello World" app.
+   port               = 80
+   protocol           = "HTTP"
+   target_group_index = 0
+  }
+ ]
+
+ target_groups = [
+  {
+   backend_port         = local.container_port
+   backend_protocol     = "HTTP"
+   target_type          = "ip"
+  }
+ ]
+}
